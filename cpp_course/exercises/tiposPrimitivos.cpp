a3188b2b062d631af5c1cfc7a0c007f5ea2656b9
Lucas <lucasbeninca@proton.me>
2025-09-24 07:07:24 -0300

    tiposPrimitivos e seus tamanhos

--- cpp_course/exercises/tiposPrimitivos.cpp
+++ cpp_course/exercises/tiposPrimitivos.cpp
@@ -11,7 +11,17 @@ int main (){
 // MODIFICADORES DO TIPO PRIMITIVO INT SHORT E UNSIGNED (NUMERO SEM SINAL +/-)
    unsigned short int age = 30;
     cout << age << endl;
+    short int n1 = 1;
+    long int n2 = 2;
+    unsigned int n3 = 3;
+    long long int n4 = 4;
 
+    cout << sizeof(char) << " bytes: " << endl;
+    cout << sizeof(short) << " bytes: " << endl;
+    cout << sizeof(int) << " bytes: " << endl;
+    cout << sizeof(long) << " bytes: " << endl;
+    cout << sizeof(long long) << " bytes: " << endl;
+    cout << sizeof(double) << " bytes: " << endl;
 
     return 0;
 
